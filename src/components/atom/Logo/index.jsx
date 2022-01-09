b10dce0c664c982ee1f99658cc4e524b32a733cb
Anthony <anthony560@hotmail.com>
2022-01-09 10:26:57 -0400

    add logo

---
+++ src/components/atom/Logo/index.jsx
@@ -0,0 +1,10 @@
+export const Logo = () => {
+  return (
+    <div className=" flex flex-col w-36 text-right">
+      <p className="font-nunito text-3xl font-black text-primary h-7 ">
+        HAMMER
+      </p>
+      <p className="font-inter text-lg text-primary leading-4 ">Insurance</p>
+    </div>
+  );
+};
